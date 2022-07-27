22789a8a76eb7156dbc89ef867b2c4f178e32fc8
hafyze <hafizmatqwer@gmail.com>
2022-07-27 18:04:14 +0800

    new page

--- app/src/main/java/com/ebookfrenzy/musicapp/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/musicapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.ebookfrenzy.musicapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick() {
+        TextView username = findViewById(R.id.userName);
+        TextView password = findViewById(R.id.password);
+
+        Button loginBtn = findViewById(R.id.buttonLogin);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    //admin and admin
+
+
 }
